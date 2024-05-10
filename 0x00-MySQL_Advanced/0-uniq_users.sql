23a455daa7b88cefde1131df9a5d1ec2658dc72b
Salah Eddine <shen.real@live.fr>
2024-05-10 03:46:05 -1200

    done tasks

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- script that creates a table users following requirements
-CREAT TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	name VARCHAR(255)
+	email VARCHAR(225) UNIQUE NOT NULL,
+	name VARCHAR(225)
 )
