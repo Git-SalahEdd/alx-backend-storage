9aae9aa173061f7234c77a322dfed569d2de59c5
Salah Eddine <shen.real@live.fr>
2024-05-15 08:17:58 -1200

    done tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -70,3 +70,18 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Retrieve data from the cache using the specified key."""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """Retrieve a string from the cache using the specified key."""
+        return str(self._redis.get(key))
+
+    def get_int(self, key: str) -> int:
+        """Retrieve an integer from the cache using the specified key."""
+        return int(self._redis.get(key))
